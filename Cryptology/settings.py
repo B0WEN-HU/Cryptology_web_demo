cf2470aba9a9506723719e5d57a8a3bbb747106f
Bowen Hu <hu_bowen@outlook.com>
2019-03-23 14:22:19 +0800

    initial

--- Cryptology/settings.py
+++ Cryptology/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@hd(tq5z=)j#i=-ha2s)yn%l0rkz)q^xtir$lfoq87xp*vs@#v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.182','127.0.0.1','passworder','www.passworder.com']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','www.passworder.com']
 
 
 # Application definition
